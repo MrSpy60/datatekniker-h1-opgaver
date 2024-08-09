ac7a6d0b37765816b7edb4f677dbf38623b688dc
Martin Mølvadgaard Svensson <62698@edu.sde.dk>
2024-08-09 11:05:02 +0200

    Added spaces to Basic assigments for readability

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -13,29 +13,36 @@ namespace Basic
 
             //Add two numbers
             Console.WriteLine(AddAndMultiply(2, 4, 5));
+            Console.WriteLine();
 
             //Celsius to Fahrenheit
             Console.WriteLine(CtoF(0));
             Console.WriteLine(CtoF(100));
             Console.WriteLine(CtoF(-300));
+            Console.WriteLine();
 
             //Elementary operations
             Console.WriteLine(ElementaryOperations(3, 8));
+            Console.WriteLine();
 
             //Is result the same
             Console.WriteLine(IsResultTheSame(2 + 2, 2 * 2));
             Console.WriteLine(IsResultTheSame(9 / 3, 16 - 1));
+            Console.WriteLine();
 
             //Modulo operations
             Console.WriteLine(ModuloOperations(8,5,2));
+            Console.WriteLine();
 
             //The cube of
             Console.WriteLine(CubeOf(2));
             Console.WriteLine(CubeOf(-5.5));
+            Console.WriteLine();
 
             //Swap two numbers
             Console.WriteLine(SwapTwoNumbers(87, 45));
             Console.WriteLine(SwapTwoNumbers(-13, 2));
+            Console.WriteLine();
         }
 
         static int AddAndMultiply(int a, int b, int c) 
