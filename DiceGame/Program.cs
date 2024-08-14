8a262a4a44f335d3927f2f22825fd3da406ceab9
Martin Mølvadgaard Svensson <62698@edu.sde.dk>
2024-08-14 12:19:37 +0200

    Added DiceLib (Class library that adds a dice class with a roll function) Added DiceGame (small program that rolls x number of dice until all hit natural sixes)

---
+++ DiceGame/Program.cs
@@ -0,0 +1,65 @@
+﻿using DiceLib;
+
+namespace DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+            int numberOfDice = 0;
+            List<Dice> diceBag = new List<Dice>();
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Today we are rolling dice.");
+            Console.WriteLine("");
+            Console.WriteLine("We are going to be rolling until all the dice hit natural 6's");
+            while (true)
+            {
+                bool isRollingDice = true;
+                int counter = 0;
+
+                Console.WriteLine("Pick how many dice we are going to roll:");
+                userInput = Console.ReadLine();
+                try
+                {
+                    numberOfDice = int.Parse(userInput);
+                }catch (Exception)
+                {
+                    Console.WriteLine("ERROR: Could not convert the input to an Integer");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    continue;
+                }
+                Console.WriteLine($"Adding {numberOfDice} Dice to the dicebag");
+                for (int i = 0; i < numberOfDice; i++)
+                {
+                    diceBag.Add(new Dice());
+                }
+                while (isRollingDice)
+                {
+                    int numberOfSixes = 0;
+                    foreach (Dice dice in diceBag)
+                    {
+                        dice.RollDie();
+                        if (dice.LastRoll == 6)
+                        {
+                            numberOfSixes++;
+                        }
+                    }
+                    if (numberOfSixes == numberOfDice)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine($"It took {counter} tries to get all natural sixes with {numberOfDice} dice.");
+                        break;
+                    }
+                    counter++;
+                    if (counter % 100000 == 0)
+                    {
+                        Console.Write($"\rCurrently rolling, tried {counter} times");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DiceLib/Dice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceLib
+{
+    public class Dice
+    {
+        private int _NumberOfSides;
+        public int NumberOfSides
+        {
+            get { return _NumberOfSides; }
+        }
+        private int _LastRoll;
+        public int LastRoll
+        {
+            get { return _LastRoll; }
+        }
+        private Random rnd = new Random();
+
+        public Dice()
+        {
+            _NumberOfSides = 6;
+        }
+
+        public Dice(int numberOfSides)
+        {
+            _NumberOfSides = numberOfSides;
+        }
+
+        public int RollDie()
+        {
+            _LastRoll = rnd.Next(1,NumberOfSides +1);
+            return LastRoll;
+        }
+    }
+}
